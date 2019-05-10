f88f9bcbd150a84feb5f5b8fd33a5894cf8ffdfc
hideokamoto <hide.okamoto@digitalcube.jp>
2019-05-10 19:32:11 +0900

    feat: seo and icons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,6 +16,13 @@ const TemplateWrapper = ({ children }) => {
         <meta name="description" content={description} />
 
         <link
+          rel="icon"
+          type="image/png"
+          href="/img/favicon-32x32.png"
+          sizes="32x32"
+        />
+        {/*
+        <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/img/apple-touch-icon.png"
@@ -23,27 +30,22 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="icon"
           type="image/png"
-          href="/img/favicon-32x32.png"
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
           href="/img/favicon-16x16.png"
           sizes="16x16"
         />
-
         <link
           rel="mask-icon"
           href="/img/safari-pinned-tab.svg"
           color="#ff4400"
         />
+        */}
+
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta property="og:image" content="/img/og-image.jpg" />
+        <meta property="og:image" content="/img/assets/twitter_header_photo_2.png" />
       </Helmet>
       <Navbar />
       <div>{children}</div>
