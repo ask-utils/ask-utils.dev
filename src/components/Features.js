eabfb865097ab8f0d2ef2cb192a130713cbf20fa
hideokamoto <hide.okamoto@digitalcube.jp>
2019-05-13 10:27:38 +0900

    products pages

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,6 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
+const TitleWithUrl = ({item}) => {
+  if (!item.url) return item.title
+  return (
+    <a href={item.url} target="_blank" rel="noopener noreferrer">
+      {item.title}
+    </a>
+  )
+}
+
+const InstallCMD = ({item}) => {
+  if (!item.cmd) return null;
+  return (
+    <pre>
+      <code>{item.cmd}</code>
+    </pre>
+  )
+}
+
 const FeatureGrid = ({ gridItems }) => (
   <div className="columns is-multiline">
     {gridItems.map(item => {
@@ -20,7 +38,10 @@ const FeatureGrid = ({ gridItems }) => (
                   <PreviewCompatibleImage imageInfo={item} />
                 </div>
                 ): null}
-                <h1>{item.title}</h1>
+                <h1>
+                  <TitleWithUrl item={item} />
+                </h1>
+                <InstallCMD item={item} />
             </div>
             <p>{item.text}</p>
           </section>
